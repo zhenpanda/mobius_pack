391fba890426d5b25703357e5592d9abb02a0648
Zhenting <zhenpanda@users.noreply.github.com>
2021-02-27 16:20:56 -0500

    popup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
+
+import Popup from 'reactjs-popup';
 import $ from 'jquery';
 import { useEffect } from 'react';
 import './assets/css/app.css';
+import 'react-transitions/dist/animations.css';
+
+import cubeIcon from './assets/icons/cube2.png';
+import itemsRef from './assets/ref/items.png';
 
 function App() {
 
   let rowCount = [];
   for(let r=0; r<80; r++) { rowCount.push(r) };
 
+  // configuration of map
   const dotPosList = [
-    { x: 14, y: 9, z: "cross"},
-    { x: 27, y: 25, z: "cross"}
+    { x: 14, y: 9, z: "circle-1-color"},
+    { x: 22, y: 3, z: "circle-2-color"}
   ]
 
   const boxClicker = (x,y) => {
     alert(x+"-"+y)
   }
+
   const drawBox = (x,y) => {
 
     if((x === 14 && y === 9)) {
 
       return(
-        <div className="box target-pos" key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y}>
+        <div className="box target-pos circle-1-color pulse" key={x+"-"+y} id={x+"-"+y}>
+          <div className="dotted-box" onClick={() => $("#wow-box-open").click()} >
 
+          </div>
         </div>
       )
     }
-    if((x === 27 && y === 25)) {
+    if((x === 22 && y === 3)) {
       return(
-        <div className="box target-pos" key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y}>
+        <div className="box target-pos circle-2-color" key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y}>
 
         </div>
       )
@@ -48,7 +58,7 @@ function App() {
     })
 
   }
-
+  // draw line
   const adjustLine = (from, to, line) => {
 
     var fT = from.offsetTop  + from.offsetHeight/2;
@@ -87,22 +97,102 @@ function App() {
     line.style.height = H + 'px';
   }
   
+  let items = [0,1,2,3,4,5,6,7,8];
+  let wowItems = [0,1,2,3,4,5,6,7,8];
+
+  // popup box modal 
+  const mobiusBox = (items) => (
+    <Popup trigger={<div id="mobius-open"></div>} modal nested>
+      {close => (
+        <div className="modal">
+          <div className="header"> mobius pack</div>
+          <div className="mobius-pack-box">
+
+            {/* <img src={itemsRef} alt="" className="items-ref-box" /> */}
+            <div className="pack-grid">
+              {items.map((i) => {
+                return(
+                  <div className="item-slot" key={"item-slot-"+i}>
+                  </div>
+                )
+              })}
+            </div>
+
+          </div>
+          <div className="actions">
+            <div className="close-btn" onClick={() => close()}>close</div>
+          </div>
+        </div>
+      )}
+    </Popup>
+  );
+  // trade box modal 
+  const wowBox = (wowItems, packItems) => {
+    return(
+      <Popup trigger={<div id="wow-box-open"></div>} modal nested>
+        {close => (
+          <div className="modal">
+            <div className="header"> MOVE ITEMS FROM GAME TO WALLET</div>
+
+            <div className="trade-box-wrap">
+
+              <div className="wow-pack-box">
+              <div className="pack-grid">
+                  {wowItems.map((i) => {
+                    return(
+                      <div className="item-slot" key={"wow-slot-"+i}>
+                      </div>
+                    )
+                  })}
+                  </div>
+              </div>
+              
+              <div className="move-item-wrap">
+                <div className="move-item-btn" onClick={() => close()}>SAVE TRANSFER</div>
+              </div>
+                
+              <div className="mobius-pack-box">
+                <div className="pack-grid">
+                  {packItems.map((i) => {
+                    return(
+                      <div className="item-slot" key={"item-slot-"+i}>
+                      </div>
+                    )
+                  })}
+                  </div>
+              </div>
+            
+            </div>
+            
+          </div>
+        )}
+      </Popup>
+
+    )
+  }
+
+  // page init line drawing and cube drawing 
   useEffect(() => {
     
-    adjustLine(document.getElementById('14-9'), document.getElementById('27-25'),document.getElementById('line'));
+    adjustLine(document.getElementById('14-9'), document.getElementById('22-3'),document.getElementById('line'));
 
   })
 
   return (
     <div className="app-bg">
-
+      <div className="cube-icon-wrap" onClick={() =>  $("#mobius-open").click()}>
+        <img src={cubeIcon} alt="" className="cube-icon" />
+      </div>
       <div className="grid" id="content">
         {rowCount.map((r) => {
           return drawRow(r);
         })}
         <div id="line"></div>
       </div>
-
+      
+      {mobiusBox(items)}
+      
+      {wowBox(wowItems, items)}
     </div>
   );
 }
