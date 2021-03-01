f731c8fd728a02a688c7370d2903faec216a2138
Zhenting <zhenpanda@users.noreply.github.com>
2021-03-01 18:45:48 -0500

    sure

--- src/App.js
+++ src/App.js
@@ -1,56 +1,97 @@
 
 import Popup from 'reactjs-popup';
 import $ from 'jquery';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './assets/css/app.css';
 import 'react-transitions/dist/animations.css';
 import ReactTooltip from 'react-tooltip';
 
 import cubeIcon from './assets/icons/cube2.png';
 // import itemsRef from './assets/ref/items.png';
-import swordItemPic from './assets/ref/item_pic/item_9.png';
 import emptyItemPic from './assets/ref/item_pic/empty.png';
+import swordItemPic from './assets/ref/item_pic/item_9.png';
+import appleItemPic from './assets/ref/item_pic/item_1.png';
+import eyeItemPic from './assets/ref/item_pic/item_2.png';
+import helmItemPic from './assets/ref/item_pic/item_3.png';
+import gobItemPic from './assets/ref/item_pic/item_4.png';
+import shieldItemPic from './assets/ref/item_pic/item_5.png';
+import potionItemPic from './assets/ref/item_pic/item_6.png';
+import bagItemPic from './assets/ref/item_pic/item_7.png';
+import bombItemPic from './assets/ref/item_pic/item_8.png';
 
 function App() {
 
   let rowCount = [];
   for(let r=0; r<80; r++) { rowCount.push(r) };
+  
+  const [ itemSlot, setItemSlot ] = useState(0);
+  const [ myItemSlot, setMyItemSlot ] = useState(emptyItemPic);
 
   // configuration of map
-  const dotPosList = [
-    { x: 14, y: 9, z: "circle-1-color"},
-    { x: 22, y: 3, z: "circle-2-color"}
+  const dotPosListOne = [
+    { x: 14, y: 9, c: "box target-pos circle-1-color pulse", },
+    { x: 22, y: 3, c: "box target-pos circle-2-color", },
+    { x: 46, y: 3, c: "box target-pos circle-4-color", },
+    { x: 58, y: 12, c: "box target-pos circle-4-color", },
+    { x: 59, y: 12, c: "box target-pos circle-5-color", },
+    { x: 51, y: 12, c: "box target-pos circle-5-color", },
+    { x: 23, y: 1, c: "box target-pos circle-2-color", },
+    { x: 9, y: 28, c: "box target-pos circle-3-color", },
+    { x: 21, y: 24, c: "box target-pos circle-3-color", },
+    { x: 37, y: 32, c: "box target-pos circle-3-color",  },
+  ];
+  const linePosListOne = [
+    { s: "14-9", e: "22-3", l: "line-1" }, 
+    { s: "46-3", e: "22-3", l: "line-2" },
+    { s: "46-3", e: "58-12", l: "line-3" },
+    { s: "58-12", e: "59-12", l: "line-4" },
+    { s: "59-12", e: "51-12", l: "line-5" },
+    { s: "51-12", e: "23-1", l: "line-6" },
+    { s: "23-1", e: "9-28", l: "line-7" },
+    { s: "23-1", e: "21-24", l: "line-8" },
+    { s: "23-1", e: "37-32", l: "line-9" },
   ]
 
-  const boxClicker = (x,y) => {
-    alert(x+"-"+y)
-  }
-
   const drawBox = (x,y) => {
+    
+    const boxClicker = (x,y) => {
+      // alert(x+"-"+y)
+    }
 
-    if((x === 14 && y === 9)) {
+    let drawSpecial = false;
+    let drawObj = {};
+    let itemImgInd = 0;
 
-      return(
-        <div className="box target-pos circle-1-color pulse" key={x+"-"+y} id={x+"-"+y}>
-          <div className="dotted-box" onClick={() => $("#game-box-open").click()} >
+    dotPosListOne.map((dot, ind) => {
+      if(dot.x === x && dot.y === y) {
+        console.log(x+"-"+y);
+        drawSpecial = true;
+        drawObj = dot;
+        itemImgInd = ind;
+      }
+      return null;
+    })
 
+    if(drawSpecial) {
+      
+      return (
+        <div className={drawObj.c} key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y} key={x+"-"+y}>
+          <div className="dotted-box" onClick={() => { 
+              $("#game-box-open").click();
+              setItemSlot(itemImgInd);
+            }}>
           </div>
         </div>
       )
-    }
-    if((x === 22 && y === 3)) {
-      return(
-        <div className="box target-pos circle-2-color" key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y}>
+    
+    }else{
 
+      return (
+        <div className="box" key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y}>
         </div>
       )
+
     }
-
-    return (
-      <div className="box" key={x+"-"+y} onClick={() => {boxClicker(x,y)}} id={x+"-"+y}>
-
-      </div>
-    )
 
   }
   const drawRow = (r) => {
@@ -61,6 +102,7 @@ function App() {
     })
 
   }
+
   // draw line
   const adjustLine = (from, to, line) => {
 
@@ -101,18 +143,12 @@ function App() {
   }
   
   // items imgs
-  let items = [
-    {"pic": swordItemPic},
-    1,2,3,4,5,6,7,8
-  ];
-  let gameItems = [
-    {"pic": swordItemPic},
-    1,2,3,4,5,6,7,8
-  ];
+  let myItems = [ swordItemPic, emptyItemPic, emptyItemPic, emptyItemPic, emptyItemPic, emptyItemPic, emptyItemPic, emptyItemPic, emptyItemPic ];
+  let gameItems = [ appleItemPic, eyeItemPic, helmItemPic, gobItemPic, shieldItemPic, potionItemPic, bagItemPic, bombItemPic, emptyItemPic ];
 
   const displayToolTip = () => {
-    return(
-      <ReactTooltip type='light' place="right" effect="float">
+    return (
+      <ReactTooltip type='light' place="right" effect="float" id="happyFace">
         <div className="item-description-box">
           <div className="item-title">[ Sword of Fighting ]</div>
           <div className="item-world">Game build 37415 Retail Version 9.0.2</div>
@@ -123,7 +159,7 @@ function App() {
           <div className="item-divider" />
           <span className="item-detail">game_asset_Id: 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d</span>
           <div className="item-divider" />
-          <span className="item-detail-text">Are you a fighter? Then this is the weapon for you. Note that this sword is usable by Priests as it is not a slashing or piercing weapon.</span>
+          <span className="item-detail-text">Famous_owner: HolyPriests Pro-Gamer-1337. The person who controls this sword controls the ppower of fighting.</span>
         </div>
       </ReactTooltip>
     )
@@ -134,16 +170,16 @@ function App() {
     <Popup trigger={<div id="mobius-open"></div>} modal nested>
       {close => (
         <div className="modal">
-          <div className="header"> mobius pack</div>
+          <div className="header">mobius pack</div>
           <div className="mobius-pack-box">
 
             <div className="pack-grid">
               {items.map((i) => {
-                if(i.pic) {
+                if(i) {
 
                   return(
-                    <div className="item-slot" key={"item-slot-"+i} data-tip={"item-slot-"+i}>
-                      <img src={i.pic} alt="" className="item-img" />
+                    <div className="item-slot" key={"item-slot-"+i} data-tip data-for='happyFace'>
+                      <img src={i} alt="" className="item-img" />
                     </div>
                   )
 
@@ -170,22 +206,34 @@ function App() {
   );
 
   // trade box modal 
-  const gameBox = (gameItems, packItems) => {
+  const gameBox = (gameItems, myItems) => {
     return(
       <Popup trigger={<div id="game-box-open"></div>} modal nested>
         {close => (
           <div className="modal">
-            <div className="header"> MOVE ITEMS FROM GAME TO WALLET</div>
+            <div className="header"> MOVE ITEMS [ GAME - WALLET ]</div>
 
             <div className="trade-box-wrap">
 
               <div className="game-pack-box">
               <div className="pack-grid">
-                  {gameItems.map((i) => {
-                    return(
-                      <div className="item-slot" key={"game-slot-"+i}>
-                      </div>
-                    )
+                  {gameItems.map((i ,ind) => {
+                    if(ind === 0) {
+                      return(
+                        <div className="item-slot" key={"item-slot-"+ind} onClick={() => {
+                          setItemSlot(8);
+                          setMyItemSlot(i)
+                        }}>
+                          <img src={gameItems[itemSlot]} alt="" className="item-img" />
+                        </div>
+                      )
+                    }else{
+                      return(
+                        <div className="item-slot" key={"game-slot-"+ind}>
+                          <img src={emptyItemPic} alt="" className="item-img" />
+                        </div>
+                      )
+                    }
                   })}
                   </div>
               </div>
@@ -197,21 +245,31 @@ function App() {
               <div className="mobius-pack-box">
                 <div className="pack-grid">
 
-                  {items.map((i) => {
-                    if(i.pic) {
-
+                  {myItems.map((i, ind) => {
+                    if(ind === 0) {
+                      
                       return(
-                        <div className="item-slot" key={"item-slot-"+i} data-tip={"item-slot-"+i}>
-                          <img src={i.pic} alt="" className="item-img" />
+                        <div className="item-slot" key={"item-slot-"+i} data-tip={"item-slot-"+ind}>
+                          <img src={i} alt="" className="item-img" />
                         </div>
                       )
 
-                    }else{
+                    }else if(ind === 1) {
+
                       return(
-                        <div className="item-slot" key={"item-slot-"+i}>
+                        <div className="item-slot" key={"item-slot-"+i} data-tip={"item-slot-"+ind}>
+                          <img src={myItemSlot} alt="" className="item-img" />
+                        </div>
+                      )
+                      
+                    }else{
+
+                      return(
+                        <div className="item-slot" key={"item-slot-"+ind}>
                           <img src={emptyItemPic} alt="" className="item-img" />
                         </div>
                       )
+
                     }
                   })}
 
@@ -219,7 +277,6 @@ function App() {
               </div>
             
             </div>
-            {displayToolTip()}
           </div>
         )}
       </Popup>
@@ -230,25 +287,35 @@ function App() {
   // page init line drawing and cube drawing 
   useEffect(() => {
     
-    adjustLine(document.getElementById('14-9'), document.getElementById('22-3'),document.getElementById('line'));
+    // draw line 1
+    linePosListOne.map((line) => {
+      adjustLine(document.getElementById(line.s), document.getElementById(line.e), document.getElementById(line.l));
+      return null;
+    })
 
   })
 
   return (
     <div className="app-bg">
+      
       <div className="cube-icon-wrap" onClick={() =>  $("#mobius-open").click()}>
         <img src={cubeIcon} alt="" className="cube-icon" />
       </div>
+      
       <div className="grid" id="content">
+        
         {rowCount.map((r) => {
           return drawRow(r);
         })}
-        <div id="line"></div>
+        {linePosListOne.map((l) => {
+          return <div id={l.l} key={l.l+"key"} className="line-style-one" />
+        })}
+
       </div>
       
-      {mobiusBox(items)}
+      {mobiusBox(myItems)}
       
-      {gameBox(gameItems, items)}
+      {gameBox(gameItems, myItems)}
 
     </div>
   );
